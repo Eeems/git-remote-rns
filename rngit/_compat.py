0728628e6982864f124a18e1f983130863ec2380
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-04-17 15:15:19 -0600

    Add missing ignore

--- rngit/_compat.py
+++ rngit/_compat.py
@@ -5,6 +5,6 @@ if sys.version_info < (3, 12):
     from typing_extensions import override  # pyright: ignore[reportUnreachable]
 
 else:
-    from typing import override
+    from typing import override  # pyright: ignore[reportUnreachable]
 
 __all__ = ["override"]
