be4feaa0d0f572a530bcfce5d9978968df2620de
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-04-19 00:03:47 -0600

    allow failed startups to retry for fuzz too

--- rngit/__main__.py
+++ rngit/__main__.py
@@ -1,23 +1,30 @@
 import sys
+from collections.abc import Callable
 from typing import NoReturn
 
+def _exec(fn: Callable[[], None]) -> NoReturn:
+    res = fn()
+    if "--verbose" in sys.argv:
+        print(f"Exit code: {res}", file=sys.stderr)
+
+    sys.exit(res)
 
 def client() -> NoReturn:
     from .client import main as _client  # noqa: PLC0415
 
-    sys.exit(_client())
+    _exec(_client)
 
 
 def server() -> NoReturn:
     from .server import main as _server  # noqa: PLC0415
 
-    sys.exit(_server())
+    _exec(_server)
 
 
 def web() -> NoReturn:
     from .web import main as _web  # noqa: PLC0415
 
-    sys.exit(_web())
+    _exec(_web)
 
 
 __all__ = ["client", "server", "web"]
